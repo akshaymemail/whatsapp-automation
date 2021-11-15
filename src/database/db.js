733afcd65abd211277653974847e2978788a0d8a
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-11-15 21:27:28 +0530

    add localhost

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 import MySQL from "mysql2";
 
 const db = MySQL.createConnection({
-  host: "localhost",
+  host: "164.52.207.204",
   user: "ankit",
   password: "1232245895",
   database: "whatsapp"
