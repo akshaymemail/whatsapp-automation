15ba2ca5a33011aa77e2601c581e05fcf41473eb
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-11-15 14:22:53 +0530

    db credentials

--- src/database/db.js
+++ src/database/db.js
@@ -2,9 +2,9 @@ import MySQL from "mysql2";
 
 const db = MySQL.createConnection({
   host: "localhost",
-  user: "root",
-  //password: "1232245895",
-  database: "employees"
+  user: "ankit",
+  password: "1232245895",
+  database: "whatsapp"
 });
 
 export default db;
